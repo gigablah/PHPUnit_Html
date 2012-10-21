1c4bbba07bef22f447f6af4d161c03acb7ff94ed
Chris Heng <bigblah@gmail.com>
2012-10-21 23:18:17 +0800

    Suppress favicon

--- src/templates/bootstrap/header.php
+++ src/templates/bootstrap/header.php
@@ -3,8 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title>Unit Test Report: <?PHP echo htmlentities($title); ?></title>
+    <link rel="shortcut icon" href="#">
     <link rel="stylesheet" href="<?PHP echo $this->url('template/css/bootstrap.min.css'); ?>" type="text/css" media="screen" title="no title" charset="utf-8">
-	<link rel="stylesheet" href="<?PHP echo $this->url('template/css/main.css'); ?>" type="text/css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="<?PHP echo $this->url('template/css/main.css'); ?>" type="text/css" media="screen" title="no title" charset="utf-8">
   </head>
   <body>
     <header style="padding-top: 40px">
